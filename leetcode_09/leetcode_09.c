b42cc9f02a8e83fb456f0f306b9eaa72e8a02c42
BeiShan <623945900@qq.com>
2024-01-25 21:06:41 +0800

    [新增] 完成判断回文数

--- leetcode_09/leetcode_09.c
+++ leetcode_09/leetcode_09.c
@@ -1,5 +1,45 @@
-#include <stdio>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int isPalindrome(int x);
+
+int main()
+{
+    srand(time(NULL));
+
+    int signal, temp_x;
+    for (int idx = 0; idx < 100; idx++)
+    {
+        temp_x = rand() % 1000;
+        signal = isPalindrome(temp_x);
+        if (signal == 1)
+        {
+            printf("the num %d is Palindrom\n", temp_x);
+        }
+    }
+}
 
 int isPalindrome(int x)
 {
+    if (x < 0)
+        return 0;
+    int temp = x;
+    int reverseX = 0;
+    while (temp)
+    {
+        for (int idx = 0; idx < 9; idx++)
+        {
+            if ((0x7fffffff - reverseX / (idx + 1)) < reverseX)
+                return 0;
+            reverseX += reverseX / (idx + 1);
+        }
+        reverseX += temp % 10;
+        temp = temp / 10;
+    }
+    if (x == reverseX)
+    {
+        return 1;
+    }
+    return 0;
 }
