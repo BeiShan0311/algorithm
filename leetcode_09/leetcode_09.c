b2cbcceb2be8734861e16f4d2c38a1c6e1cb1a22
BeiShan <623945900@qq.com>
2024-01-25 21:55:35 +0800

    [新增] 新增方法二，只计算一半回文数字

--- leetcode_09/leetcode_09.c
+++ leetcode_09/leetcode_09.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <math.h>
 
+#define NUM_TEN 10
 int isPalindrome(int x);
+int isPalindrome2(int x);
 
 int main()
 {
@@ -12,7 +15,7 @@ int main()
     for (int idx = 0; idx < 100; idx++)
     {
         temp_x = rand() % 1000;
-        signal = isPalindrome(temp_x);
+        signal = isPalindrome2(temp_x);
         if (signal == 1)
         {
             printf("the num %d is Palindrom\n", temp_x);
@@ -20,25 +23,79 @@ int main()
     }
 }
 
-int isPalindrome(int x)
+int isPalindrome2(int x)
 {
     if (x < 0)
         return 0;
     int temp = x;
-    int reverseX = 0;
+    int location = 0;
+    // 计算数字的位数
+    while (temp)
+    {
+        temp /= 10;
+        location++;
+    }
+    if (location == 1)
+        return 1;
+    int reverseX = x % 10;
+    temp = x / 10;
+    for (int idx = 1; idx < location / 2; idx++)
+    {
+        reverseX *= 10;
+        reverseX += temp % 10;
+        temp /= 10;
+    }
+    temp = 1;
+    for (int idx = 0; idx < (location + 1) / 2; idx++)
+    {
+        temp *= 10;
+    }
+    x = x / temp;
+    if (x == reverseX)
+        return 1;
+    return 0;
+
+    // int temp = x / 10;
+    // int reverseX = x % 10;
+    // while (temp)
+    // {
+    //     reverseX *= 2;
+    //     for (int idx = 0; idx < NUM_TEN / 2 - 1; idx++)
+    //     {
+    //         if ((0x7fffffff - reverseX / ((idx + 1) * 2) * 2) < reverseX)
+    //             return 0;
+    //         reverseX += reverseX / ((idx + 1) * 2) * 2;
+    //     }
+    //     reverseX += temp % NUM_TEN;
+    //     temp /= NUM_TEN;
+    // }
+    // if (!(x ^ reverseX))
+    // {
+    //     return 1;
+    // }
+    // return 0;
+}
+
+// 方法一
+int isPalindrome(int x)
+{
+    if (x < 0)
+        return 0;
+    int temp = x / 10;
+    int reverseX = x % 10;
     while (temp)
     {
         reverseX *= 2;
-        for (int idx = 0; idx < 4; idx++)
+        for (int idx = 0; idx < NUM_TEN / 2 - 1; idx++)
         {
             if ((0x7fffffff - reverseX / ((idx + 1) * 2) * 2) < reverseX)
                 return 0;
             reverseX += reverseX / ((idx + 1) * 2) * 2;
         }
-        reverseX += temp % 10;
-        temp = temp / 10;
+        reverseX += temp % NUM_TEN;
+        temp /= NUM_TEN;
     }
-    if (x == reverseX)
+    if (!(x ^ reverseX))
     {
         return 1;
     }
